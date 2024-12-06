f608843a39fcc6216507c64272b82700c101bae7
Rishav Roshan <88714527+Rish200@users.noreply.github.com>
2024-12-07 02:58:16 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_id = request.form['user_id']
+        user = User(user_id)
+        login_user(user)
+        return redirect(url_for('protected'))
+    return render_template('login.html')
+
+@app.route('/protected')
+@login_required
+def protected():
+    return f'Logged in as: {current_user.id}'
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
